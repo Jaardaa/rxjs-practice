fee50f982b0afd5af94cbbab7ba902a391db51a2
Jarda Bílek <jarbilek@gmail.com>
2022-02-05 20:04:13 +0100

    Reactively redesigned

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, noop, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import { createHttpObservable } from '../common/util';
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { interval, noop, Observable, of, timer } from "rxjs";
+import {
+  catchError,
+  delayWhen,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
   selector: "home",
@@ -11,12 +17,9 @@ import { createHttpObservable } from '../common/util';
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
-
-
-
+  advancedCourses$: Observable<Course[]>;
 
   constructor() {}
 
@@ -27,16 +30,16 @@ export class HomeComponent implements OnInit {
       map((res) => Object.values(res["payload"]))
     );
 
-    courses$.subscribe(
-      courses => {
-        this.beginnerCourses = courses.filter(course => course.category == 'BEGINNER');
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "BEGINNER")
+      )
+    );
 
-        this.advancedCourses = courses.filter(
-          (course) => course.category == "ADVANCED"
-        );
-      },
-      noop,
-      () => console.log("completed")
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "ADVANCED")
+      )
     );
   }
 }
